767cd2b5fbaf0d1668623ec9a471ecee932c5286
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-12-28 18:47:46 +0545

    Update 3. animation example

--- lib/general_flutter/3.animation_example.dart
+++ lib/general_flutter/3.animation_example.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
 
 class AnimationsHomePage extends StatelessWidget {
   final List<Item> items = [
@@ -8,7 +8,7 @@ class AnimationsHomePage extends StatelessWidget {
     Item('Item 3', 'Description for Item 3', Icons.bookmark),
   ];
 
-  const AnimationsHomePage({super.key});
+  AnimationsHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
