1a4cd455606d3f7ec2fbad2c87a3d4e90f9df603
Aadarsh Shumsher Kunwar <aadarshkunwar8@gmail.com>
2025-01-17 10:16:21 +0545

    chore: upgrade gradle and jdk to 21

--- lib/media_player/viewmodels/player_view_model.dart
+++ lib/media_player/viewmodels/player_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
+
 import '../models/media_file.dart';
 
 class PlayerViewModel extends ChangeNotifier {
